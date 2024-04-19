bd9d4c392ed9bb7330a23f38c2bce4638a7eaf71
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-04-20 00:15:51 +0500

    first commit

--- app/src/main/java/com/example/leaarn_kotlin_for_android/HomeActivity/HomeActivity.kt
+++ app/src/main/java/com/example/leaarn_kotlin_for_android/HomeActivity/HomeActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.leaarn_kotlin_for_android.R
+import com.example.leaarn_kotlin_for_android.databinding.ActivityHomeBinding
+import com.example.leaarn_kotlin_for_android.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
