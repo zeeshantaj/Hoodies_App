c9012a5855ac9f92ca11236aa3eb3b80d2819742
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-04-19 20:22:17 +0500

    first commit

--- app/src/main/java/com/example/leaarn_kotlin_for_android/Fragments/LoginFragment.kt
+++ app/src/main/java/com/example/leaarn_kotlin_for_android/Fragments/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.leaarn_kotlin_for_android.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,27 +8,37 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.leaarn_kotlin_for_android.Animation.FragmentUtils
+import com.example.leaarn_kotlin_for_android.HomeActivity.HomeActivity
 import com.example.leaarn_kotlin_for_android.R
 import com.example.leaarn_kotlin_for_android.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding =  FragmentLoginBinding.inflate(layoutInflater, container, false)
+        auth = FirebaseAuth.getInstance()
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
         binding.loginBtn.setOnClickListener {
-            val name = binding.nameEd.text
+            val name = binding.emailEd.text
+            val pass = binding.passwordEd.text
             if (name?.isEmpty()!!){
-                Toast.makeText(activity,"please Enter the name", Toast.LENGTH_SHORT).show()
-                setAnimation(binding.nameEd)
+                binding.emailEd.error = "Name is empty"
+                setAnimation(binding.emailEd)
+                return@setOnClickListener
+            }
+            if (pass?.isEmpty()!!){
+                binding.passwordEd.error = "Name is empty"
+                setAnimation(binding.passwordEd)
                 return@setOnClickListener
             }
             if (!binding.checkBox.isChecked){
@@ -35,11 +46,18 @@ class LoginFragment : Fragment() {
                 Toast.makeText(activity,"please check the ", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            Toast.makeText(activity,"\n"+binding.checkBox.isChecked,
-                Toast.LENGTH_SHORT).show()
-            activity?.let { it1 -> FragmentUtils.changeFragment(it1.supportFragmentManager,R.id.mainFragmentContainer,CreateAccountFragment()) }
+
+            auth.signInWithEmailAndPassword(name.toString(),pass.toString()).addOnSuccessListener {
+                val intent = Intent(activity, HomeActivity::class.java)
+                startActivity(intent)
+            }
 
         }
+        binding.createAccTxt.setOnClickListener {
+            activity?.let { it1 -> FragmentUtils.changeFragment(it1.supportFragmentManager,R.id.mainFragmentContainer,CreateAccountFragment()) }
+        }
+
+
     }
     private fun setAnimation(view: View) {
         val shake = android.view.animation.AnimationUtils.loadAnimation(activity, R.anim.shake_animation)
