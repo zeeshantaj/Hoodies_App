f3fc2bc3528074ab2e2bd1d1ba5008240852332d
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-04-20 23:33:16 +0500

    main ui functionality

---
+++ app/src/main/java/com/example/leaarn_kotlin_for_android/HomeActivity/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.leaarn_kotlin_for_android.HomeActivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.leaarn_kotlin_for_android.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
