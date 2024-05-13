572a53d93289cb67119a5377547cdf8a359b6e0e
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-05-13 23:44:29 +0500

    optimization commit

--- app/src/main/java/com/example/leaarn_kotlin_for_android/Favourite/FragmentFavourite.kt
+++ app/src/main/java/com/example/leaarn_kotlin_for_android/Favourite/FragmentFavourite.kt
@@ -1,17 +1,22 @@
 package com.example.leaarn_kotlin_for_android.Favourite
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.leaarn_kotlin_for_android.Models.ProductModel
 import com.example.leaarn_kotlin_for_android.databinding.FragmentFavouriteBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class FragmentFavourite: Fragment() {
     private lateinit var binding: FragmentFavouriteBinding
-    private var productList = mutableListOf<ProductModel>()
+    private var favItemList = mutableListOf<ProductModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,10 +28,30 @@ class FragmentFavourite: Fragment() {
     }
 
     override fun onStart() {
+        getFavItems()
         super.onStart()
     }
     private fun getFavItems(){
+        val firebase = FirebaseAuth.getInstance()
+        val uid = firebase.uid
         val database = FirebaseDatabase.getInstance()
-        val productRef = database.getReference("products").child("men")
+        val productRef = database.getReference("favourite").child(uid.toString())
+        productRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                favItemList.clear()
+                if (snapshot.exists()){
+                    for (favItemSnapshot in snapshot.children){
+                        // get fav items
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("MyApp","Error ${error.message}")
+            }
+
+        })
+
     }
 }
