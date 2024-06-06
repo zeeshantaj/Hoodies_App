dcbbe30b0b0afb862c91a290adc5abc6ef300019
zeeshantaj <91092210+zeeshantaj@users.noreply.github.com>
2024-06-06 23:41:36 +0500

    Update MethodUtils.kt

--- app/src/main/java/com/example/leaarn_kotlin_for_android/Utils/MethodUtils.kt
+++ app/src/main/java/com/example/leaarn_kotlin_for_android/Utils/MethodUtils.kt
@@ -2,7 +2,7 @@ package com.example.leaarn_kotlin_for_android.Utils
 
 class MethodUtils {
     companion object{
-        public fun(){
+        public fun getCalculatedAmmount(){
             
     }
 }
